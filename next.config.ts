794cebda72962d8019735a1be0d4d292c855692a
Ayat <ayathosam20@gmail.com>
2026-07-15 12:10:55 +0300

    add chromium and puppeteer for reports

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/api/cron/sales-report/route": ["./node_modules/@sparticuz/chromium/**"],
+      "/api/cron/collection-report/route": ["./node_modules/@sparticuz/chromium/**"],
+    },
+  },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
